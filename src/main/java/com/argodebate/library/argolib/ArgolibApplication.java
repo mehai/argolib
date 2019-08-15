b762f475396bb046fe4735897f3f728ac24d9b3c
mehai <mihaizanfir98@gmail.com>
2019-08-15 13:40:52 +0300

    add initial project structure

---
+++ src/main/java/com/argodebate/library/argolib/ArgolibApplication.java
@@ -0,0 +1,13 @@
+package com.argodebate.library.argolib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgolibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgolibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argodebate/library/argolib/ArgolibApplicationTests.java
@@ -0,0 +1,16 @@
+package com.argodebate.library.argolib;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArgolibApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
