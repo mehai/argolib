82489cb6f2c5845b4833da461aef1a16958945ae
mehai <mihaizanfir98@gmail.com>
2019-08-16 16:35:56 +0300

    add basic security in memory layer

---
+++ src/main/java/com/argodebate/library/argolib/controllers/SecurityController.java
@@ -0,0 +1,18 @@
+package com.argodebate.library.argolib.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "logout";
+    }
+}
